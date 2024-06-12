8abb78c961c19853f66f87f1e3314460842475eb
KoushickBora <koushickbora1122@gmail.com>
2024-06-12 15:03:08 +0530

    version 1.1

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -21,19 +21,19 @@ export default function Main() {
             </div> 
             <div className="cards">
                 <div className="card">
-                    <p>hello</p>
+                    <p>Explore new thing with Gemini</p>
                     <img src={assets.compass_icon} alt="" />
                 </div>
                 <div className="card">
-                    <p>hello</p>
+                    <p>Get new ideas for you project</p>
                     <img src={assets.bulb_icon} alt="" />
                 </div>
                 <div className="card">
-                    <p>hello</p>
+                    <p>Helps you in drafting official paperworks</p>
                     <img src={assets.message_icon} alt="" />
                 </div>
                 <div className="card">
-                    <p>hello</p>
+                    <p>Help you to debug your code</p>
                     <img src={assets.code_icon} alt="" />
                 </div>
             </div>
@@ -61,7 +61,7 @@ export default function Main() {
                   {input?<img onClick={()=>onSent()} src={assets.send_icon} alt="" />:null}  
                 </div>
                 <p className="bottom-info">
-                    its is not accurate
+                    Note:The results are generated on the based of machine learning, it might not be accurate( all features are now working now)
                 </p>
             </div>
         </div>
